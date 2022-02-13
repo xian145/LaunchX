82b1f3f01f384e2049d85cb81502e16bbed7e46d
xian145 <abraham.arizmendi.grijalva@gmail.com>
2022-02-12 22:52:59 -0700

    Corrección kata 2
    - Se agrego carpeta para el modulo 10
    - Se subieron ss correspondientes a la kata2
    - se elimino los archivos originales en la kata2
    - se volvieron a crear los archivos de la kata2 junto con un notebook donde se explica que fue lo que se hizo junto con los ss

--- Ejercicios_modulo_2/env/Scripts/activate.bat
+++ Ejercicios_modulo_2/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\xian1\Dropbox\Innovaccion\Ejercicios_sesion_3\env
+set VIRTUAL_ENV=C:\Users\xian1\Dropbox\LaunchX\Ejercicios_modulo_2\env
 
 if not defined PROMPT set PROMPT=$P$G
 
